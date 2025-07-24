031d42aca7a75cd8fb8787eea5c928eceb280715
felipeassis97 <felipeassis97@gmail.com>
2025-07-25 08:07:28 +1000

    feat: initial commit

---
+++ src/main/java/com/felipeassisdev/walletpp/WalletppApplication.java
@@ -0,0 +1,13 @@
+package com.felipeassisdev.walletpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletppApplication.class, args);
+	}
+
+}
